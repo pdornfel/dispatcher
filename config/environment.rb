d5db59cb417ce21fc9351df0ad68b92d8b9918cd
Johnny Boursiquot <jboursiquot@gmail.com>
2014-01-06 09:26:36 -0500

    Removed ActionMailer config

--- config/environment.rb
+++ config/environment.rb
@@ -4,12 +4,4 @@ require File.expand_path('../application', __FILE__)
 # Initialize the Rails application.
 Dispatcher::Application.initialize!
 
-ActionMailer::Base.smtp_settings = {
-  :user_name => ENV['SENDGRID_USERNAME'],
-  :password => ENV['SENDGRID_PASSWORD'],
-  :domain => 'johnny-dispatch.herokuapp.com',
-  :address => 'smtp.sendgrid.net',
-  :port => 587,
-  :authentication => :plain,
-  :enable_starttls_auto => true
-}
+
